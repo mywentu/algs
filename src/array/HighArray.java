cf002d6fecb8d88f7d75315a7329ab12b4e361e6
'mywentu' <'wentu1996@163.com'>
2017-03-15 13:46:57 +0800

    add high array demo

---
+++ src/array/HighArray.java
@@ -0,0 +1,65 @@
+package array;
+
+public class HighArray {
+	private long[] a;
+	private int nElems;
+	public HighArray(int max) {
+		a = new long[max];
+		nElems = 0;
+	}
+	public boolean find(long serachKey) {
+		int j;
+		for(j=0; j<nElems; j++)
+			if(a[j] == serachKey)
+				break;
+		if(j == nElems) return false;
+		else return true;
+	}
+	public void insert(long value) {
+		a[nElems] = value;
+		nElems ++;
+	}
+	public boolean delete(long value) {
+		int j;
+		for(j=0; j<nElems; j++){
+			if(value == a[j])
+				break;
+		}
+		if(j==nElems) {
+			return false; //not found value
+		} else {
+			for(int k=j; k<nElems; k++)
+				a[k] = a[k+1];
+			nElems--;
+			return true;
+		}
+		
+	}
+	public void display() {
+		for(int j=0; j<nElems; j++){
+			System.out.println(a[j]+ " ");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/array/HighArrayApp.java
@@ -0,0 +1,27 @@
+package array;
+
+public class HighArrayApp {
+	public static void main(String[] args) {
+		int maxSize = 100;
+		HighArray arr;
+		arr = new HighArray(maxSize);
+		arr.insert(89);
+		arr.insert(32);
+		arr.insert(34);
+		arr.insert(3);
+		arr.insert(9);
+		arr.display();
+		System.out.println("---------insert test-----------------");
+		int searchKey = 34;
+		if(arr.find(searchKey)){
+			System.out.println(" Found " + searchKey);
+		} else{
+			System.out.println(" Not Found ");
+		}
+		System.out.println("---------find test-----------------");
+		arr.delete(0);
+		arr.delete(3);
+		arr.display();
+		System.out.println("---------delete test-----------------");	
+	}
+}
